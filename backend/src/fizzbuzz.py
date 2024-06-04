76d9acfe37010370f9ffd7bde242521bcc80ac7f
DogisRiki <dogisriki1122@gmail.com>
2024-06-04 09:46:20 +0900

    first commit

---
+++ backend/src/fizzbuzz.py
@@ -0,0 +1,16 @@
+from module.add import double_number
+
+
+def fizzbuzz(num):
+    if num % 15 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
+
+
+print(double_number(1))
+print(fizzbuzz(3))
